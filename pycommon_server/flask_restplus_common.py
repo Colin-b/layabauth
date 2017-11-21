4b907dbb5816374d6447e6b6c721d9ccf57e2540
Olivier Feys <olivier.feys@gdfsuez.com>
2017-11-21 19:19:35 +0100

    small refactor to ease the reading

--- pycommon_server/flask_restplus_common.py
+++ pycommon_server/flask_restplus_common.py
@@ -86,7 +86,9 @@ class LogRequestDetails:
 
     def __call__(self, *func_args, **func_kwargs):
         from flask import request, has_request_context
-        if self.log:
+        if not self.log:
+            return self.request_method(*func_args, **func_kwargs)
+        else:
             args_name = list(OrderedDict.fromkeys(inspect.getfullargspec(self.request_method)[0] + list(func_kwargs.keys())))
             args_dict = OrderedDict(list(zip(args_name, func_args)) + list(func_kwargs.items()))
             stats = {'func_name': self.request_method.__qualname__}
@@ -95,27 +97,28 @@ class LogRequestDetails:
             if has_request_context():
                 stats.update(dict([(f'request.args.{k}', v[0]) if len(v) == 1 else (k, v) for k, v in dict(request.args).items()]))
                 stats.update({f'request.headers.{k}':v for k,v in dict(request.headers).items()})
-        start = time.perf_counter()
-        try:
-            ret =  self.request_method(*func_args, **func_kwargs)
-        except Exception as e:
-            if has_request_context():
-                stats['request.data'] = request.data
-            exc_type, exc_value, exc_traceback = sys.exc_info()
-            trace = traceback.extract_tb(exc_traceback)
-            #don't want the current frame in the traceback
-            if len(trace) > 1:
-                trace = trace[1:]
-            trace.reverse()
-            trace_summary = '/'.join([os.path.splitext(os.path.basename(tr.filename))[0]+'.'+tr.name for tr in trace])
-            tb = [{'line':tr.line,'file': tr.filename, 'lineno':tr.lineno, 'func':tr.name} for tr in trace]
-            stats.update({'error.summary':trace_summary, 'error.class':exc_type.__name__, 'error.msg':str(exc_value), 'error.traceback':traceback.format_exc()})
-            logger.critical(stats)
-            raise e
+            start = time.perf_counter()
+            try:
+                ret =  self.request_method(*func_args, **func_kwargs)
+            except Exception as e:
 
-        stats['request.processing_time'] = time.perf_counter() - start
-        logger.info(stats)
-        return ret
+                if has_request_context():
+                    stats['request.data'] = request.data
+                exc_type, exc_value, exc_traceback = sys.exc_info()
+                trace = traceback.extract_tb(exc_traceback)
+                #don't want the current frame in the traceback
+                if len(trace) > 1:
+                    trace = trace[1:]
+                trace.reverse()
+                trace_summary = '/'.join([os.path.splitext(os.path.basename(tr.filename))[0]+'.'+tr.name for tr in trace])
+                tb = [{'line':tr.line,'file': tr.filename, 'lineno':tr.lineno, 'func':tr.name} for tr in trace]
+                stats.update({'error.summary':trace_summary, 'error.class':exc_type.__name__, 'error.msg':str(exc_value), 'error.traceback':traceback.format_exc()})
+                logger.critical(stats)
+                raise e
+
+            stats['request.processing_time'] = time.perf_counter() - start
+            logger.info(stats)
+            return ret
 
 
 # This will log every incoming request (do not use a method to avoid registering it twice by accident)
