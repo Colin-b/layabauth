8d8539900101d569fba560ebffdd5415b66cea9e
Olivier Feys <olivier.feys@gdfsuez.com>
2017-11-21 17:57:15 +0100

    change logging decorator to log objectas and detailled callstack/traceback in case of errors

--- pycommon_server/flask_restplus_common.py
+++ pycommon_server/flask_restplus_common.py
@@ -1,5 +1,12 @@
 import logging
 from flask_restplus import Resource, fields
+import time
+from collections import OrderedDict
+import inspect
+import sys
+import traceback
+import os
+
 
 logger = logging.getLogger(__name__)
 
@@ -75,18 +82,42 @@ class LogRequestDetails:
     """
     def __init__(self, request_method):
         self.request_method = request_method
+        self.log = "Health.get" not in request_method.__qualname__
 
-    def __call__(self, *args, **kwargs):
-        self.log_request()
-        return self.request_method(*args, **kwargs)
+    def __call__(self, *func_args, **func_kwargs):
+        from flask import request, has_request_context
+        if self.log:
+            args_name = list(OrderedDict.fromkeys(inspect.getfullargspec(self.request_method)[0] + list(func_kwargs.keys())))
+            args_dict = OrderedDict(list(zip(args_name, func_args)) + list(func_kwargs.items()))
+            stats = {'func_name': self.request_method.__qualname__}
+            stats.update(args_dict)
+            # add request args
+            if has_request_context():
+                stats.update(dict([(f'request.args.{k}', v[0]) if len(v) == 1 else (k, v) for k, v in dict(request.args).items()]))
+                stats.update({f'request.headers.{k}':v for k,v in dict(request.headers).items()})
+        start = time.perf_counter()
+        try:
+            ret =  self.request_method(*func_args, **func_kwargs)
+        except Exception as e:
+            if has_request_context():
+                stats['request.data'] = request.data
+            exc_type, exc_value, exc_traceback = sys.exc_info()
+            trace = traceback.extract_tb(exc_traceback)
+            #don't want the current frame in the traceback
+            if len(trace) > 1:
+                trace = trace[1:]
+            trace.reverse()
+            trace_summary = '/'.join([os.path.splitext(os.path.basename(tr.filename))[0]+'.'+tr.name for tr in trace])
+            tb = [{'line':tr.line,'file': tr.filename, 'lineno':tr.lineno, 'func':tr.name} for tr in trace]
+            stats.update({'error.summary':trace_summary, 'error.class':exc_type.__name__, 'error.msg':str(exc_value), 'error.traceback':traceback.format_exc()})
+            logger.critical(stats)
+            raise e
 
-    def log_request(self):
-        from flask import request
-        if self.request_method.__name__ in ['post', 'put']:
-            logger.info(f'{request}: {request.data}')
-        else:
-            logger.info(f'{request}')
+        stats['request.processing_time'] = time.perf_counter() - start
+        logger.info(stats)
+        return ret
 
 
 # This will log every incoming request (do not use a method to avoid registering it twice by accident)
 Resource.method_decorators.append(LogRequestDetails)
+
