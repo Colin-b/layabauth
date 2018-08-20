3326ee6230b4d49c73f411b6f65eb1e7a8f651d7
WW5328 <jeremy.langlois@external.engie.com>
2018-08-20 17:19:31 +0200

    Update dependencies

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,12 @@ List all changes in various categories:
 * Bug fixes
 * Known issues
 
+## Version 3.3.3 (2018-08-20) ##
+
+### Bug fixes ###
+
+- Update dependencies to latest version.
+
 ## Version 3.3.2 (2018-08-10) ##
 
 ### Bug fixes ###
--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup, find_packages
 
 from pycommon_server._version import __version__
+
 setup(
     name='pycommon_server',
     version=__version__,
@@ -15,8 +16,9 @@ setup(
     ],
     extras_require={
         'testing': [
-            'pycommon-test==1.9.0',
+            'pycommon-test==1.9.1',
             'pysmb==1.1.25',
+            'nose'
         ],
         'authentication': [
             'oauth2helper==1.1.1',
