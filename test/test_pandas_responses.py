0d7b950d7f5c9b0de9d82c1716a90669140d7b80
Kevin Thackray <kevin.thackray@external.engie.com>
2019-01-14 16:09:12 +0100

    remove deps to excel

--- test/test_pandas_responses.py
+++ test/test_pandas_responses.py
@@ -1,14 +1,12 @@
 import datetime
 import json
-from io import BytesIO
 
 import pandas as pd
 from flask import Flask
 from flask_testing import TestCase
-from openpyxl import load_workbook
-from pycommon_test import mock_now, revert_now
+from pycommon_test import mock_now
 
-from pycommon_server.pandas_responses import dataframe_as_response, dataframe_as_excelresponse
+from pycommon_server.pandas_responses import dataframe_as_response
 
 
 class HttpUtilTest(TestCase):
@@ -29,33 +27,3 @@ class HttpUtilTest(TestCase):
         self.assertEqual(expected_data, json.loads(response.data))
         self.assertEqual('application/json', response.headers.get('Content-type'))
         self.assert200(response)
-
-    def test_dataframe_to_excel_200(self):
-        keys = ['key1', 'key2']
-        data = [{'key1': 'row11', 'key2': 'row12'}, {'key1': 'row21', 'key2': datetime.datetime.now()}]
-        revert_now()
-        expected_data = [{'key1': 'row11', 'key2': 'row12'},
-                         {'key1': 'row21', 'key2': datetime.datetime(2018, 10, 11, 0, 0)}]
-        mock_now()
-        df = pd.DataFrame.from_dict(data)
-        response = dataframe_as_excelresponse(df, file_name='test.xlsx', sheet_name='test_sheet')
-        self.assertEqual('application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-                         response.headers.get('Content-type'))
-        self.assertEqual('attachment; filename=test.xlsx', response.headers.get('Content-Disposition'))
-        self.assert200(response)
-        wb = load_workbook(filename=BytesIO(response.data), read_only=True)
-        ws = wb['test_sheet']
-        current_row = 0
-        real_data = []
-        for row in ws.rows:
-            current_col = 0
-            real_row = {}
-            for cell in row:
-                ## skip the header
-                if current_row != 0:
-                    real_row[keys[current_col]] = cell.value
-                current_col += 1
-            current_row += 1
-            if real_row:
-                real_data.append(real_row)
-        self.assertEqual(expected_data, real_data)
