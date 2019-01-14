d8a9e9e7a7792e849b5dddfe154b5527c003c628
Kevin Thackray <kevin.thackray@external.engie.com>
2019-01-14 16:32:52 +0100

    hardcode date

--- test/test_pandas_responses.py
+++ test/test_pandas_responses.py
@@ -4,15 +4,11 @@ import json
 import pandas as pd
 from flask import Flask
 from flask_testing import TestCase
-from pycommon_test import mock_now
 
 from pycommon_server.pandas_responses import dataframe_as_response
 
 
 class HttpUtilTest(TestCase):
-
-    def setUp(self):
-        mock_now()
 
     def create_app(self):
         app = Flask(__name__)
@@ -20,8 +16,9 @@ class HttpUtilTest(TestCase):
         return app
 
     def test_dataframe_200(self):
-        data = [{'key1': 'row11', 'key2': 'row12'}, {'key1': 'row21', 'key2': datetime.datetime.now()}]
-        expected_data = [{'key1': 'row11', 'key2': 'row12'}, {'key1': 'row21', 'key2': '2018-10-11T15:05:05.663Z'}]
+        data = [{'key1': 'row11', 'key2': 'row12'},
+                {'key1': 'row21', 'key2': datetime.datetime(2018, 10, 11, 15, 0, 0)}]
+        expected_data = [{'key1': 'row11', 'key2': 'row12'}, {'key1': 'row21', 'key2': '2018-10-11T15:00:00.000Z'}]
         df = pd.DataFrame.from_dict(data)
         response = dataframe_as_response(df)
         self.assertEqual(expected_data, json.loads(response.data))
