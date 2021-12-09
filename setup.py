c806092aa45121dedd1bf08de8766d17af498b55
Colin-b <Colin-b@users.noreply.github.com>
2021-12-09 23:49:14 +0100

    Switch to github actions

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Topic :: Software Development :: Build Tools",
     ],
     keywords=["flask", "starlette", "auth"],
